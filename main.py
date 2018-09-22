9e368eb4917b0b00274f9fafb3388718fa725a4d
haopinuuan <37677792+haopinuuan@users.noreply.github.com>
2018-09-22 20:16:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+
+
 for i in range(1,10):
-    for j in range(1,i+1):
-        if i==1:
-           print(end='        '*8) 
-        formula = '{0:1}×{1:1}={2:<2}'.format(i,j,i*j)
-        print(formula, end=' ')
+    for j in range(i,10):
+        formula = '{0:1}×{1:1}={2:<3}'.format(i,j,i*j)
+        print(formula, end='')
     print()
-    print(end='        '*(8-i))
+    print(end='        '*i)
